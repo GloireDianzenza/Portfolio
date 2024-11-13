a8b0504465fff0560b4043abf151c4d6f6a3af2d
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-11-13 16:25:15 +0100

    sub form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 function Footer() {
+
+    /**
+     * 
+     * @param {import('react').BaseSyntheticEvent} event 
+     */
+    function handleSubmit(event){
+        event.preventDefault();
+        window.location.reload();
+    }
+
     return (<footer className='mt-20'>
         <div className='left'>
             <p>Les utilisateurs abonnées auront droit à une réduction du prix de prestation de 20% !</p>
         </div>
         <div className='right'>
-
+            <form onSubmit={handleSubmit}>
+                <input type='email' placeholder='Email...'  name='email'/>
+                <button type='submit'>S'inscrire</button>
+            </form>
         </div>
     </footer>);
 }
