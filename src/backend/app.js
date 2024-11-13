d7ea05acf2394392196baa589cb5c8750daefdc7
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-11-13 16:34:58 +0100

    backend

---
+++ src/backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const {sequelize,DataTypes} = require("./init");
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(cors());
+app.use(express.json());
+
+async function connect(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+connect()
+sequelize.sync();
+
+module.exports = app;
