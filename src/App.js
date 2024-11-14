4ef3f569f97e063bed15eba4bcc8784afd5a0d72
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-11-14 11:46:06 +0100

    flip card

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Icon } from "./components/index.ts";
 import emailjs from "@emailjs/browser";
 import { useEffect } from 'react';
 import "./css/responsive.css";
+import Ui from "./assets/ui-ux-representations-with-laptop.jpg";
 
 const FadeIn = styled.div`animation: 1.2s ${keyframes `${fadeIn}`} linear`;
 const SlideInLeft = styled.div`animation: 1.2s ${keyframes `${slideInLeft}`} ease-in-out`;
@@ -46,20 +47,20 @@ function App() {
         <div id='home'></div>
         <Header/>
         <main>
-            <h1>Gloire Dianzenza, développeur Web Full-Stack français</h1>
-            <p className='intro'>Je suis un développeur Web vivant en Île-de-France et ayant 4 ans d'expérience, avec notamment un an de BTS SIO.</p>
+            <h1>Gloire Dianzenza, Développeur Web Full-Stack</h1>
+            <p className='intro'>Je suis un développeur Web résidant en Île-de-France et programmant depuis 4 ans, avec notamment un an de BTS SIO.</p>
           <div id='about' className='mt-52 flex flex-col items-center pt-[10vh]'>
             <h1>Au sujet de moi</h1>
-            <p className='intro'>Je suis un jeune développeur Full-Stack qui a acquis toutes les expériences que j'ai grâce à mes études de lycée et d'université et également via une formation. <br/><br/> J'ai eu le privilège de collaborer avec une entreprise pour mettre à jour son contenu et design. Le développement Web n'est pas qu'un travail pour moi. Je suis passionné par tout ce qui concerne l'informatique, la programmation et les jeux vidéo.</p>
+            <p className='intro'>Je suis un jeune développeur Full-Stack ayant acquis de nombreuses compétences dans le domaines. <br/><br/> J'ai eu le privilège de collaborer avec une entreprise pour mettre à jour son contenu et design. Le développement Web n'est pas qu'un travail pour moi. Je suis passionné par tout ce qui concerne l'informatique, la programmation et les jeux vidéo.</p>
             <SlideInLeft className='w-full px-6'>
               <div className='skills-list mt-10'>
                 <div className='skill-group'>
                   <label>HTML/CSS</label>
-                  <hr style={{width:"90%"}}/>
+                  <hr style={{width:"100%"}}/>
                 </div>
                 <div className='skill-group'>
                   <label>Javascript</label>
-                  <hr style={{width:"95%"}}/>
+                  <hr style={{width:"100%"}}/>
                 </div>
                 <div className='skill-group'>
                   <label>PHP</label>
@@ -75,7 +76,7 @@ function App() {
                 </div>
                 <div className='skill-group'>
                   <label>Python</label>
-                  <hr style={{width:"100%"}}/>
+                  <hr style={{width:"70%"}}/>
                 </div>
               </div>
             </SlideInLeft>
@@ -84,19 +85,44 @@ function App() {
             <h1>Mes services</h1>
             <div className='services'>
               <div className='service'>
-                <h2>Design de sites Web</h2>
-                <p className='intro'>Mise à jour de l'apparences des sites web.</p>
+                <div className='service-inner'>
+                  <div className='service-front'>
+                    <img alt='' src={Ui}/>
+                  </div>
+                  <div className='service-back'>
+                    <h2>Design et mise à jour de sites web</h2>
+                  </div>
+                </div>
               </div>
               <div className='service'>
+                <div className='service-inner'>
+                  <div className='service-front'>
+                    <img alt='' src={Ui}/>
+                  </div>
+                  <div className='service-back'>
+                    <h2>Attribution de liens vers d'autres pages, sites ou réseaux sociaux</h2>
+                  </div>
+                </div>
+              </div>
+              <div className='service'>
+                <div className='service-inner'>
+                  <div className='service-front'>
+                    <img alt='' src={Ui}/>
+                  </div>
+                  <div className='service-back'>
+                    <h2>Réécriture, Ajout de texte, médias, etc...</h2>
+                  </div>
+                </div>
+              </div>
+              {/* <div className='service'>
                 <h2>Assignation de liens</h2>
                 <p className='intro'>Attribuer des liens hypertexte pour d'autres sites ou des réseaux sociaux.</p>
               </div>
               <div className='service'>
                 <h2 className='capitalize'>écriture de contenu</h2>
                 <p className='intro'>Que ce soit réécriture, ajout de texte, importation de médias, etc...</p>
-              </div>
+              </div> */}
             </div>
-            <p className='intro mt-16'>Sachez que le prix de chaque prestation sera toujours négocié dès la première rencontre.</p>
           </div>
           <div id='works' className='mt-52 flex flex-col items-center pt-[10vh]'>
             <h1>Projets déjà effectués</h1>
